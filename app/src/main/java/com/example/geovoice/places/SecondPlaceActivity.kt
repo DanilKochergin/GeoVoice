88f32b2932d0f0329d4ad47c8d918c29c7e35dfd
DanilKochergin <danil2606200884@gmail.com>
2026-02-08 00:44:16 +0500

    create secondplace ui

--- app/src/main/java/com/example/geovoice/places/SecondPlaceActivity.kt
+++ app/src/main/java/com/example/geovoice/places/SecondPlaceActivity.kt
@@ -6,15 +6,12 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.geovoice.R
-import com.example.geovoice.databinding.ActivityFirstPlaceBinding
 
-class FirstPlaceActivity : AppCompatActivity() {
-    lateinit var binding: ActivityFirstPlaceBinding
+class SecondPlaceActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        binding = ActivityFirstPlaceBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(R.layout.activity_second_place)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
