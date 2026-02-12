1154639a91f0a810462cbb6c51aa9aaf72f9226e
DanilKochergin <danil2606200884@gmail.com>
2026-02-12 23:11:36 +0500

    Added a second and third activity, added a memory feature for saving visited activities, and a transition to the СongradulationsActivity.

--- app/src/main/java/com/example/geovoice/MainActivity.kt
+++ app/src/main/java/com/example/geovoice/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.geovoice
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
@@ -7,7 +8,10 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.geovoice.databinding.ActivityMainBinding
+import com.example.geovoice.places.CongradulationsActivity
 import com.example.geovoice.places.FirstPlaceActivity
+import com.example.geovoice.places.SecondPlaceActivity
+import com.example.geovoice.places.ThirdPlaceActivity
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
@@ -22,8 +26,41 @@ class MainActivity : AppCompatActivity() {
             insets
         }
         binding.tv1.setOnClickListener {
+            markVisited("place1")
             val intent = Intent(this, FirstPlaceActivity::class.java)
             startActivity(intent)
+        }
+        binding.tv2.setOnClickListener {
+            markVisited("place2")
+            val intent = Intent(this, SecondPlaceActivity::class.java)
+            startActivity(intent)
+        }
+        binding.tv3.setOnClickListener {
+            markVisited("place3")
+            val intent = Intent(this, ThirdPlaceActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        checkQuestionCompletion()
+    }
+    private fun markVisited(key: String){
+        val prefs = getSharedPreferences("QuestPrefs", Context.MODE_PRIVATE)
+        prefs.edit().putBoolean(key, true).apply()
+    }
+
+    private fun checkQuestionCompletion(){
+        val prefs = getSharedPreferences("QuestPrefs", Context.MODE_PRIVATE)
+        val p1 = prefs.getBoolean("place1", false)
+        val p2 = prefs.getBoolean("place2", false)
+        val p3 = prefs.getBoolean("place3", false)
+        if (p1 && p2 && p3){
+            val intent = Intent(this, CongradulationsActivity::class.java)
+            startActivity(intent)
+            prefs.edit().clear().apply()
+                finish()
         }
     }
 
--- app/src/main/java/com/example/geovoice/places/CongradulationsActivity.kt
+++ app/src/main/java/com/example/geovoice/places/CongradulationsActivity.kt
@@ -1,30 +1,21 @@
 package com.example.geovoice.places
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.example.geovoice.MainActivity
 import com.example.geovoice.R
-import com.example.geovoice.databinding.ActivityFirstPlaceBinding
 
-class FirstPlaceActivity : AppCompatActivity() {
-    lateinit var binding: ActivityFirstPlaceBinding
+class CongradulationsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        binding = ActivityFirstPlaceBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(R.layout.activity_congradulations)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-        binding.backbtn.setOnClickListener {
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
         }
     }
 }
